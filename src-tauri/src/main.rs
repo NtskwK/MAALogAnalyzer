90627d754df9f9b645c1bc294f6c94fc22e728ad
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2025-12-11 05:22:05 +0800

    fix(rust): 修修

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(debug_assertions)]
 use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
+        .setup(|_app| {
             #[cfg(debug_assertions)]
             {
-                let window = app.get_window("main").unwrap();
+                let window = _app.get_window("main").unwrap();
                 window.open_devtools();
             }
             Ok(())
